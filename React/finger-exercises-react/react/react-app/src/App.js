d3c610fa5bb2a74e00354360dc343977568489bf
anaospinarojas <anisospina@gmail.com>
2018-10-18 14:42:02 -0500

    Practicando

--- React/finger-exercises-react/react/react-app/src/App.js
+++ React/finger-exercises-react/react/react-app/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class App extends React.Component{
+class App extends React.Component {
+  render(){
+    return <Title text="123456" />
+  }
+}
 
-    render(){
-      return <h1>Hello world</h1>
+const Title = (props) => <h1>Title: {props.text}</h1>
+
+Title.PropTypes = {
+  text(props, propName, Component){
+    if (!(propName in props)){
+      return new Error('Missing ${propName}')
     }
-
-
+    if(props[propName].length < 6){
+      return new Error('${propName} was too short')
+    }
+  }
 }
 
 export default App
+
+
