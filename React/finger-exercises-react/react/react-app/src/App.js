55214dc6024652f6d04465f36fabd1ff4506feb4
anaospinarojas <anisospina@gmail.com>
2018-10-20 17:26:53 -0500

    ENV I - create-react-app

--- React/finger-exercises-react/react/react-app/src/App.js
+++ React/finger-exercises-react/react/react-app/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-render(){
+class App extends Component {
+  render(){
+    
+  }
+}
 
-}
-}
-  
-  export default Wrapper
-  
+
+
+export default Wrapper
