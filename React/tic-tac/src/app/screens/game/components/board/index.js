3b474b45b7234c0a04e53dca7aeb27c5fc2c35aa
anaospinarojas <anisospina@gmail.com>
2018-10-25 10:45:15 -0500

    Lifting State Up, Again

--- React/tic-tac/src/app/screens/game/components/board/index.js
+++ React/tic-tac/src/app/screens/game/components/board/index.js
@@ -2,88 +2,43 @@ import React, { Component } from 'react';
 import style from '../board/style.scss';
 import Square from '@square';
 
-   class Board extends Component {
+class Board extends Component {
 
-    state = {
-      squares : Array(9).fill(null),
-      xIsNext: true,
-    }
+renderSquare(i) {
+return( 
+  <Square value={this.props.squares[i]} 
+  onClick={() => 
+                this.props.onClick(i)} 
+  />
+);
+}
 
-    handleClick(i){
-      const squares = this.state.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      squares[i] = this.state.xIsNext ? 'X':'O';
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
-      });
-    }
+render() {
 
-    renderSquare(i) {
-      return( 
-      <Square value={this.state.squares[i]} 
-      onClick={() => this.handleClick(i)} 
-      />
-      );
-    }
-    
-    render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner) {
-        status = 'Winner : ' + winner;
-      } else {
-        status = 'Next Player : ' + (this.state.xIsNext ? 'X':'O');
-      }
-      
-      
-      return (
-        <React.Fragment>
-          <div className={style.status}>{status}</div>
-          <div className={style.row}>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          </div>
-          <div className={style.row}>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          </div>
-          <div className={style.row}>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-          </div>
-        </React.Fragment>
-        );
-      }
-    }
+return (
+<React.Fragment>
+  <div className={style.status}>{status}</div>
+    <div className={style.row}>
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
+    </div>
+    <div className={style.row}>
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+    </div>
+    <div className={style.row}>
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+  </div>
+</React.Fragment>
+);
+}
+}
 
 
-    function calculateWinner(squares) {
-      const lines = [
-          [0,1,2],
-          [3,4,5],
-          [6,7,8],
-          [0,3,6],
-          [1,4,7],
-          [2,5,8],
-          [0,4,8],
-          [2,4,6],
-      
-      ];
-      for (let i = 0; i<lines.length; i++){
-          const [a,b,c] = lines[i];
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-              return squares[a];
-          }
-      }
-      return null;
-  }
-  
 
 
-    export default Board;
+export default Board;
--- React/tic-tac/src/app/screens/game/index.js
+++ React/tic-tac/src/app/screens/game/index.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
 import style from '../game/style.scss';
 import Board from '@board';
+import calculateWinner from '../../../util.js';
+
 
 class Game extends Component {
+
+state = {
+  history : [{squares: Array(9).fill(null)}],
+  xIsNext : true,
+}
+
+handleClick(i) {
+  const history = this.state.history;
+  const current = history[history.length -1];
+  const squares = current.squares.slice();
+
+  if(calculateWinner(squares) || squares[i]) {
+    return;
+  }
+  squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+  this.setState({
+    history: history.concat([{ squares: squares }]),
+    xIsNext: !this.state.xIsNext,
+  });
+}
+
     render() {
+
+      const history = this.state.history;
+      const current = history[history.length -1]
+      const winner = calculateWinner(current.squares);
+
+      let status;
+      if (winner) {
+        status = 'Winner:' + winner;
+      } else{
+        status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div className={style.game}>
         <div className={style.gameBoard}>
-        <Board />
+        <Board  squares={current.squares} onClick={(i) => this.handleClick(i)} />
         </div>
         <div className={style.gameInfo}>
-        <div>{/* status */}</div>
+        <div className={style.titulo}>{status}</div>
         <ol>{/* TODO */}</ol>
         </div>
         </div>
