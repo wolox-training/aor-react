8fb61b577e55a060e664ddf46d994cfa5de330a9
anaospinarojas <anisospina@gmail.com>
2018-10-31 14:27:54 -0500

    add variables

--- React/tic-tac/src/app/components/App/index.js
+++ React/tic-tac/src/app/components/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Header from '@components/Header';
-import Game from '@Screens/Game/';
+import Game from '@screens/Game/';
 
 class App extends Component {
   render() {
