468daf36f041959f0a19f94a510abeefdc880c2c
anaospinarojas <anisospina@gmail.com>
2018-11-22 13:22:14 -0500

    removed const empty

--- React/tic-tac/src/app/screens/Setting/ components/IconSelector/icons.js
+++ React/tic-tac/src/app/screens/Setting/ components/IconSelector/icons.js
@@ -1,11 +1,8 @@
-const state = {
-  icon: '',
-  icons: [
-    { name: 'flushed', id: 1 },
-    { name: 'grimace', id: 2 },
-    { name: 'grin-hearts', id: 3 },
-    { name: 'grin-tongue-wink', id: 4 }
-  ]
-};
+const icons = [
+  { name: 'flushed', id: 1 },
+  { name: 'grimace', id: 2 },
+  { name: 'grin-hearts', id: 3 },
+  { name: 'grin-tongue-wink', id: 4 }
+];
 
-export default state;
+export default icons;
