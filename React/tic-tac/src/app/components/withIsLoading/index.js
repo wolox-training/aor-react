c479accdb954f2a594a908d62731a5001bc2de0f
anaospinarojas <anisospina@gmail.com>
2018-11-27 17:26:36 -0500

    Deleted space between class

--- React/tic-tac/src/app/components/withIsLoading/index.js
+++ React/tic-tac/src/app/components/withIsLoading/index.js
@@ -5,16 +5,16 @@ function LoaderHOC(WrappedComponent) {
   return function wrapper(props) {
     const {userLoading} = props;
    return userLoading ? (
-      <div className={ style.skcubegrid}>
-        <div className={` ${style.skcube} ${style.skcube1} `} />
-        <div className={` ${style.skcube} ${style.skcube2} `} />
-        <div className={` ${style.skcube} ${style.skcube3} `} />
-        <div className={` ${style.skcube} ${style.skcube4} `} />
-        <div className={` ${style.skcube} ${style.skcube5} `} />
-        <div className={` ${style.skcube} ${style.skcube6} `} />
-        <div className={` ${style.skcube} ${style.skcube7} `} />
-        <div className={` ${style.skcube} ${style.skcube8} `} />
-        <div className={` ${style.skcube} ${style.skcube9} `} />
+      <div className={style.skcubegrid}>
+        <div className={`${style.skcube} ${style.skcube1}`} />
+        <div className={`${style.skcube} ${style.skcube2}`} />
+        <div className={`${style.skcube} ${style.skcube3}`} />
+        <div className={`${style.skcube} ${style.skcube4}`} />
+        <div className={`${style.skcube} ${style.skcube5}`} />
+        <div className={`${style.skcube} ${style.skcube6}`} />
+        <div className={`${style.skcube} ${style.skcube7}`} />
+        <div className={`${style.skcube} ${style.skcube8}`} />
+        <div className={`${style.skcube} ${style.skcube9}`} />
         <div className={style.title}>Loading ...</div>
       </div>) : (<WrappedComponent {...props} />); 
   };
